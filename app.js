bf81f39f826fd993f0fc1d1eb169661f8c226745
JohnRM992 <jonatanrm92@gmail.com>
2024-01-20 03:27:11 -0300

    modificacion de estilos,agrego login.html

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ app.use(express.static('public'));
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
-});
+})
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
-});
+})
 
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 })
+app.get('/login', (req,res) =>{
+    res.sendFile(__dirname + '/views/login.html');
+});
